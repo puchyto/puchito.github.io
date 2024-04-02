eeb59c211e214a37b3d098a2e5a1f49439466a90
puchyto <154814196+puchyto@users.noreply.github.com>
2024-04-02 17:25:06 -0300

    dfg

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 window.sr = ScrollReveal();
-sr.reveal('#titulo-c1', {
+sr.reveal('#titulo', {
     duration: 2000,
-    origin: 'bottom',
-    distance: '-100px'
 });
+sr.reveal('#c1',{
+    duration: 3500,
+})
+sr.reveal('#carta',{
+    duration: 5000,
+    origin: 'right',
+    distance: '400px',
+})
+sr.reveal('p',{
+    duration: 4900, 
+})
